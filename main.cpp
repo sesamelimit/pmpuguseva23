a445339fa22cbc5dfeaf9878b297de4f22708956
Masha <st089778@student.spbu.ru>
2022-05-09 18:53:55 +0300

    a

--- main.cpp
+++ main.cpp
@@ -5,14 +5,9 @@
 int main() {
     Node x;
     ifstream(fin);
-    fin.open("C:\\Users\\a9599\\CLionProjects\\second_semester\\data.txt");
+    fin.open("C:\\Users\\a9599\\CLionProjects\\PersonTest\\data.txt");
     fin >> x;
     fin.close();
-    Node y;
-    fin.open("C:\\Users\\a9599\\CLionProjects\\second_semester\\data2.txt");
-    fin >> y;
-    fin.close();
-    x.add(y);
     cout << x;
     return 0;
 }
